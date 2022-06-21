3d45ea97df22c02b8596ba9ceb643fafe4b7c0b5
Nguyen <vuxuannguyen11122001@gmail.com>
2022-06-21 09:59:55 +0700

    nguyen-1-create-db

---
+++ src/test/java/com/ces/intern/sunsama/SunsamaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ces.intern.sunsama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunsamaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
