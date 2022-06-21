02b825cbae51bfadd1a5b8231049f2096176cfab
Nguyen <vuxuannguyen11122001@gmail.com>
2022-06-21 09:59:55 +0700

    nguyen-1-create-db

---
+++ Sunsama/src/main/java/com/ces/intern/sunsama/SunsamaApplication.java
@@ -0,0 +1,13 @@
+package com.ces.intern.sunsama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunsamaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SunsamaApplication.class, args);
+    }
+
+}
