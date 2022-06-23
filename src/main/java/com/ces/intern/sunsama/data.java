865943fc7cb5d0bc8d70776eec9b83530d56fd19
Nguyen <vuxuannguyen11122001@gmail.com>
2022-06-23 17:19:29 +0700

    nguyen-2-create-read-hashtag

--- src/main/java/com/ces/intern/sunsama/data.java
+++ src/main/java/com/ces/intern/sunsama/data.java
@@ -1,4 +1,24 @@
 package com.ces.intern.sunsama;
 
+import com.ces.intern.sunsama.dto.HashtagDTO;
+import com.ces.intern.sunsama.entity.HashtagEntity;
+import com.ces.intern.sunsama.reponsitory.HashtagRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class data {
+    private  static  final Logger logger = LoggerFactory.getLogger(data.class);
+    @Bean
+    CommandLineRunner initDatabase(HashtagRepository hashtagRepository) {
+        return args -> {
+//
+//            logger.info("Preloading " + hashtagRepository.save(new HashtagEntity("UI/UX")));
+//            logger.info("Preloading " + hashtagRepository.save(new HashtagEntity("Backend")));
+//            logger.info("Preloading " + hashtagRepository.save(new HashtagEntity("Gt")));
+        };
+    }
 }
