b65db7d17ede91dcd7a45331067fdd30b4914329
NinhDuong <ninhduong25nd@gmail.com>
2022-06-23 18:11:47 +0700

    Ninh-3-Read-Create-Task

--- Sunsama/src/main/java/com/ces/intern/sunsama/SunsamaApplication.java
+++ Sunsama/src/main/java/com/ces/intern/sunsama/SunsamaApplication.java
@@ -1,7 +1,12 @@
 package com.ces.intern.sunsama;
 
+import com.ces.intern.sunsama.entity.UserEntity;
+import com.ces.intern.sunsama.repository.UserRepository;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -12,11 +17,24 @@ public class SunsamaApplication {
     public static void main(String[] args) {
         SpringApplication.run(SunsamaApplication.class, args);
     }
-@Bean
-public ModelMapper modelMapper (){
-    ModelMapper modelMapper = new ModelMapper();
-    modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
-    return modelMapper;
-}
+
+
+
+    @Bean
+    public ModelMapper modelMapper (){
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        return modelMapper;
+    }
+
+    @Bean
+    CommandLineRunner initDatabase(UserRepository userRepository) {
+        Logger log = LoggerFactory.getLogger(SunsamaApplication.class);
+        return args -> {
+            userRepository.save(new UserEntity(1L,"testUser", "123456", null));
+            userRepository.findAll()
+                    .forEach(user -> log.info("Preloaded " + user));
+        };
+    }
 
 }
---
+++ Sunsama/src/main/java/com/ces/intern/sunsama/repository/HashtagRepository.java
@@ -0,0 +1,7 @@
+package com.ces.intern.sunsama.repository;
+
+import com.ces.intern.sunsama.entity.HashtagEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HashtagRepository extends JpaRepository<HashtagEntity,Long> {
+}
