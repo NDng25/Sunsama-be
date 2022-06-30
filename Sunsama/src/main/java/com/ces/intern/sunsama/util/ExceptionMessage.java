e839310ccf8873637341eb993888ee3d7d2baf3a
NinhDuong <ninhduong25nd@gmail.com>
2022-06-30 09:41:20 +0700

    [BE-5] Delete Task

--- Sunsama/src/main/java/com/ces/intern/sunsama/util/ExceptionMessage.java
+++ Sunsama/src/main/java/com/ces/intern/sunsama/util/ExceptionMessage.java
@@ -1,15 +1,10 @@
 package com.ces.intern.sunsama.util;
 
 public enum ExceptionMessage {
-<<<<<<< HEAD
-    NOT_FOUND_HASHTAG("Not Found Hashtag"),
-    Hashtag_ALREADY_EXIST("Hashtag Already Exist");
 
-=======
-    NOT_FOUND_HASHTAG("Hashtag not found"),
-    HASHTAG_ALREADY_EXIST("Hashtag Already Exist"),
-    TASK_NOT_EXIST("This task does not exist");
->>>>>>> 9a46798 ([BE-4] Update Task- Fix code review)
+    NOT_FOUND_HASHTAG("Not Found Hashtag"),
+    TASK_NOT_EXIST("This task does not exist"),
+    HASHTAG_ALREADY_EXIST("Hashtag Already Exist");
     private String message;
     ExceptionMessage(String message){
         this.message = message;
