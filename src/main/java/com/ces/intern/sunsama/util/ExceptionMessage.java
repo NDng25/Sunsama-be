f530bdcae23c7d9c8cd248db2ca9893bcd33d6eb
NinhDuong <ninhduong25nd@gmail.com>
2022-06-30 09:41:20 +0700

    [BE-4] Update Task- Fix code review

--- src/main/java/com/ces/intern/sunsama/util/ExceptionMessage.java
+++ src/main/java/com/ces/intern/sunsama/util/ExceptionMessage.java
@@ -1,9 +1,9 @@
 package com.ces.intern.sunsama.util;
 
 public enum ExceptionMessage {
-    NOT_FOUND_HASHTAG("Not Found Hashtag"),
-    HASHTAG_ALREADY_EXIST("Hashtag Already Exist");
-
+    NOT_FOUND_HASHTAG("Hashtag not found"),
+    HASHTAG_ALREADY_EXIST("Hashtag Already Exist"),
+    TASK_NOT_EXIST("This task does not exist");
     private String message;
     ExceptionMessage(String message){
         this.message = message;
