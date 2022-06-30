ec6ac04acdb1877ed56e40a94ff72587649c4fd8
Nguyen <vuxuannguyen11122001@gmail.com>
2022-06-30 09:26:09 +0700

    nguyen-7-delete-hashtag

---
+++ src/main/java/com/ces/intern/sunsama/http/response/HashtagReponse.java
@@ -0,0 +1,13 @@
+package com.ces.intern.sunsama.http.response;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class HashtagReponse {
+    private Long id;
+    private String name;
+}
