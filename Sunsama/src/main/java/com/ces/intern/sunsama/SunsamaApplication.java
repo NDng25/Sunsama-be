c0fa17cfa75262d9a8084d070e8fc7ffae72abc4
NDng25 <ninhduong25nd@gmail.com>
2022-07-09 18:03:15 +0700

    deploy

--- Sunsama/src/main/java/com/ces/intern/sunsama/SunsamaApplication.java
+++ Sunsama/src/main/java/com/ces/intern/sunsama/SunsamaApplication.java
@@ -2,6 +2,7 @@ package com.ces.intern.sunsama;
 
 import com.ces.intern.sunsama.entity.UserEntity;
 import com.ces.intern.sunsama.repository.UserRepository;
+import org.apache.catalina.User;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.slf4j.Logger;
@@ -9,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
